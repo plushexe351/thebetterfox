b03fd24fa9f564843d2626f2d370ce5c0cbcdf70
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2026-01-12 06:24:13 +0530

    feat: scaffold app, and init settings sheet

---
+++ components/SideSheet.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Edit } from "lucide-react";
+
+type Props = {};
+
+const SideSheet = (props: Props) => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="outline" className="fixed bottom-6 right-6"><Edit/></Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default SideSheet;
---
+++ components/TheBetterFoxMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TheBetterFoxMain = (props: Props) => {
+  return (
+    <div className='border-r-accent-foreground w-2 h-screen'>Main home page</div>
+  )
+}
+
+export default TheBetterFoxMain
